7a63aed81949f51cf366bc7a53d2b853a312d215
Heloisabressanin <114985653+Heloisabressanin@users.noreply.github.com>
2026-03-20 10:26:50 +0100

    modifs

--- src/main/java/com/livraison/model/Livraison.java
+++ src/main/java/com/livraison/model/Livraison.java
@@ -1,28 +1,50 @@
 package com.livraison.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Document(collection = "livraisons")
 public class Livraison {
 
     @Id
     private String id;
-
     private String colisId;
     private String livreur;
-    private String statut; // en_attente, en_transit, livre, echec
+    private String statut;
     private LocalDateTime timestamp;
     private Localisation localisation;
     private String clientNom;
     private String adresseDest;
-    private String priorite; // normal, express
+    private String priorite;
+
+    public Livraison() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getColisId() { return colisId; }
+    public void setColisId(String colisId) { this.colisId = colisId; }
+
+    public String getLivreur() { return livreur; }
+    public void setLivreur(String livreur) { this.livreur = livreur; }
+
+    public String getStatut() { return statut; }
+    public void setStatut(String statut) { this.statut = statut; }
+
+    public LocalDateTime getTimestamp() { return timestamp; }
+    public void setTimestamp(LocalDateTime timestamp) { this.timestamp = timestamp; }
+
+    public Localisation getLocalisation() { return localisation; }
+    public void setLocalisation(Localisation localisation) { this.localisation = localisation; }
+
+    public String getClientNom() { return clientNom; }
+    public void setClientNom(String clientNom) { this.clientNom = clientNom; }
+
+    public String getAdresseDest() { return adresseDest; }
+    public void setAdresseDest(String adresseDest) { this.adresseDest = adresseDest; }
+
+    public String getPriorite() { return priorite; }
+    public void setPriorite(String priorite) { this.priorite = priorite; }
 }
--- src/main/java/com/livraison/model/Localisation.java
+++ src/main/java/com/livraison/model/Localisation.java
@@ -1,15 +1,29 @@
 package com.livraison.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Localisation {
     private double lat;
     private double lng;
     private String ville;
     private String zone;
+
+    public Localisation() {}
+
+    public Localisation(double lat, double lng, String ville, String zone) {
+        this.lat = lat;
+        this.lng = lng;
+        this.ville = ville;
+        this.zone = zone;
+    }
+
+    public double getLat() { return lat; }
+    public void setLat(double lat) { this.lat = lat; }
+
+    public double getLng() { return lng; }
+    public void setLng(double lng) { this.lng = lng; }
+
+    public String getVille() { return ville; }
+    public void setVille(String ville) { this.ville = ville; }
+
+    public String getZone() { return zone; }
+    public void setZone(String zone) { this.zone = zone; }
 }
